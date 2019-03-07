5d68f0182c0818b7d37e18a8b0228a634c00045d
arthyom <a.gonzalezgavina@ugto.mx>
2019-03-07 14:00:45 -0600

    interfaz de tutorial basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule, AlertController } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { TutorialPage } from '../pages/tutorial/tutorial';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    TutorialPage
   ],
   imports: [
     BrowserModule,
@@ -19,10 +21,12 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    TutorialPage
   ],
   providers: [
     StatusBar,
+    AlertController,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController} from 'ionic-angular';
+import { TutorialPage } from '../tutorial/tutorial';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+     public modalController: ModalController, 
+     ) {
 
   }
 
+  // show tutorial page
+  showTutorial() {
+    let modal = this.modalController.create(TutorialPage);
+    modal.present();
+  }
+  
 }
